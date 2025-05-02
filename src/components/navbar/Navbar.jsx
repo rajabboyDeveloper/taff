bfe3dcad95cbcc429ca4003f186fefe98c409b19
rajabboyDeveloper <nnaimov.rajabboy@gmail.com>
2025-05-02 17:34:01 +0500

    First code

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import style from "./Navbar.module.css";
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
---
+++ src/components/pages/stati/Stati.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Stati() {
+  return <div>Stati</div>;
+}
+
+export default Stati;
