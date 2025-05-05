5dd9663d9027ea9bda7e52d4d8785db6742c0ce5
rajabboyDeveloper <nnaimov.rajabboy@gmail.com>
2025-05-05 17:59:25 +0500

    navbar

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,75 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import style from "./Footer.module.css";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import {
+  YMaps,
+  Map,
+  Placemark,
+  FullscreenControl,
+  TrafficControl,
+} from "react-yandex-map";
 export default function Footer() {
-  return <div>Footer</div>;
+  const [info, setinfo] = useState([]);
+  useEffect(() => {
+    axios.get("https://admin.taffeine.com/api/menu").then((resp) => {
+      setinfo(resp.data.data);
+    });
+  }, []);
+  return (
+    <div className={style.footer}>
+      <div className="container">
+        <div className={style.first}>
+          <h1 className={style.title}>Будьте в курсе новостей</h1>
+          <div className={style.input_group}>
+            <input
+              type="text "
+              className={style.input}
+              placeholder="Введите адрес электронной почты"
+            />
+            <button className={style.btn}>Отправлять</button>
+          </div>
+        </div>
+        <div className={style.second}>
+          <div className={style.left}>
+            <div className={style.mini}>
+              <div className={style.colls}></div>
+              <div className={style.gmail}></div>
+              <div className={style.loc}></div>
+            </div>
+            <ul className={style.item}>
+              {info.map((item, index) => {
+                return (
+                  <li>
+                    <NavLink to={item.slug}>{item.title}</NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className={style.right}>
+            <YMaps>
+              <Map
+                defaultState={{
+                  center: [41.311151, 69.279737],
+                  zoom: 10,
+                }}
+                width="400px"
+                height="400px"
+              >
+                <Placemark geometry={[41.327526, 69.184264]} />
+                <Placemark geometry={[41.330623, 69.223106]} />
+                <FullscreenControl />
+                <TrafficControl
+                  options={{
+                    float: "right",
+                  }}
+                />
+              </Map>
+            </YMaps>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const [info, setinfo] = useState([]);
+  useEffect(() => {
+    axios.get("https://admin.taffeine.com/api/menu").then((resp) => {
+      setinfo(resp.data.data);
+    });
+  }, []);
+
+  return (
+    <div className={style.navbar}>
+      <div className="container">
+        <nav className={style.nav}>
+          <NavLink to="/" className={style.logo}>
+            <img
+              src="https://taffeine.com/_nuxt/img/logo.6335003.svg"
+              alt=""
+              className=""
+            />
+          </NavLink>
+          <ul className={style.item}>
+            {info.map((item, index) => {
+              return (
+                <li>
+                  <NavLink to={item.slug}>{item.title}</NavLink>
+                </li>
+              );
+            })}
+          </ul>
+          <div className={style.btn_group}>
+            <button className={style.btn}>Купить онлайн</button>
+            <button className={style.btn2}>Uzb</button>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
 }
